7acbd1194e8deda50e898d692a59db00e6f86a71
soenneker <jake@soenneker.com>
2026-08-29 14:16:57 -0500

    Improve public API XML documentation

--- src/Soenneker.Copper.OpenApiClientUtil/Abstract/ICopperOpenApiClientUtil.cs
+++ src/Soenneker.Copper.OpenApiClientUtil/Abstract/ICopperOpenApiClientUtil.cs
@@ -2,13 +2,16 @@ using Soenneker.Copper.OpenApiClient;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace Soenneker.Copper.OpenApiClientUtil.Abstract;
-
 /// <summary>
 /// Exposes a cached OpenAPI client instance.
 /// </summary>
 public interface ICopperOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured copper OpenAPI Client used by the Copper OpenAPI Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested copper OpenAPI Client.</returns>
     ValueTask<CopperOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
